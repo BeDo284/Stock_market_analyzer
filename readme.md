b751ce749e16f953aa3f02b766c8d86ce298bf33
Brecht Dobbelaere <brechtdobbelaere5@gmail.com>
2024-04-13 00:25:49 +0200

    change creation of the dataframes

--- readme.md
+++ readme.md
@@ -13,5 +13,16 @@
 5. crypto information
 6. commodities
 
+
+
+* Open: The price of the stock when the market opens in the morning
+* Close: The price of the stock when the market closed in the evening
+* High: Highest price the stock reached during that day
+* Low: Lowest price the stock is traded on that day
+* Volume: The total amount of stocks traded on that day
+
+put data into files because the standard API rate limit is set to 25 requests per day
+
 https://github.com/JNYH/Analysing_the_Stock_Market_with_Python/blob/main/Analysing%20the%20Stock%20Market%20p1.ipynb
 https://www.analyticsvidhya.com/blog/2021/07/stock-prices-analysis-with-python/
+https://www.datacamp.com/tutorial/lstm-python-stock-market
--- test file.py
+++ test file.py
@@ -1,23 +1,68 @@
-import matplotlib.pyplot as plt
+import json
+import os
+import seaborn as sns
 import pandas as pd
 import requests
-import seaborn as sns
+from matplotlib import pyplot as plt
 
-api_key = 'W59OQACAUZ0HS2N3'
+api_key1 = 'W59OQACAUZ0HS2N3'
+api_key2 = 'TWGWL6U2XC4X2L6C'
 
-ticker = 'MSFT'
-url1 = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TSCO.LON&outputsize=full&apikey={api_key}'
-url2 = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={api_key}'
-url3 = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={ticker}&apikey={api_key}'
+ticker = 'AAPL'
+url_daily = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={api_key2}'
+url_monthly = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol={ticker}&apikey={api_key2}'
+url_weekly = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&{ticker}=IBM&apikey={api_key2}'
+url_intra_day = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={ticker}&interval=5min&apikey={api_key2}'
 
-response = requests.get(url3)
-json_data = response.json()
-print(json_data)
-data = json_data.get('Monthly Time Series')
-df = pd.DataFrame(data)
-data_df = df.T  # .T changes the rows and columns.
-data_df.info()
-sns.set_theme(style="darkgrid")
-sns.lineplot(x=data_df.index, y=data_df['1. open'], data=data_df)
+# Define the relative path
+relative_path = 'data/intraday/intraday_stock_market_data-%s.json' % ticker
+# Get the base directory of the current Python script
+base_directory = os.path.dirname(os.path.abspath(__file__))
+# Construct the full path by joining the base directory and the relative path
+file_to_save = os.path.join(base_directory, relative_path)
 
+
+def create_file():
+    response = requests.get(url_intra_day)
+    json_data = response.json()
+    directory = os.path.dirname(file_to_save)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    with open(file_to_save, 'w', newline='') as file:
+        json.dump(json_data, file, indent=4)
+
+
+def create_dataframes():
+    meta_data = None
+    stock_data = None
+    try:
+        with open(file_to_save) as f:
+            data = json.load(f)
+        # Separate meta_data and stock_data
+        meta_data = pd.DataFrame(data['Meta Data'], index=['Meta Data']).T
+        stock_data = pd.DataFrame(data['Time Series (5min)']).T
+        #stock_data = pd.DataFrame(data['Time Series (Daily)']).T
+        # stock_data = pd.DataFrame(data['Monthly Time Series']).T
+        # .T transposes the DataFrame, so that dates become rows and attributes become columns.
+        # Display the DataFrames
+        print("Meta Data:")
+        print(meta_data)
+        print("\nStock Data:")
+        print(stock_data)
+    except Exception as e:
+        print(f'An error occurred: {e}')
+    return meta_data, stock_data
+
+
+#create_file()
+meta_data_df, stock_data_df = create_dataframes()
+
+plt.figure(figsize=(10, 6))
+sns.lineplot(data=stock_data_df['1. open'], label='Open')
+plt.title('Stock Market Data: Open')
+plt.xlabel('Date')
+plt.ylabel('Value')
+every_sixth_tick = stock_data_df.index[::6]
+plt.xticks(ticks=every_sixth_tick, rotation=45)
+plt.legend()
 plt.show()
